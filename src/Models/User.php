13efac37a3b860146b6b39395041de5d3ae44f5d
Patrik Jakab <patrikjak@gmail.com>
2025-04-07 22:17:08 +0200

    phpcs, phpstan

--- src/Models/User.php
+++ src/Models/User.php
@@ -8,6 +8,7 @@ use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
